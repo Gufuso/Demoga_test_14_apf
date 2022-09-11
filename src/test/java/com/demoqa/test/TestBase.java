6b5abc7f777d34f389d3a751afdad5f2122d6bea
Gufuso <Tedos1988@mail.ru>
2022-09-11 14:27:14 +0300

    Merge remote-tracking branch 'origin/HomeWork4' into HomeWork4

---
+++ src/test/java/com/demoqa/test/TestBase.java
@@ -0,0 +1,14 @@
+package com.demoqa.test;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+
+    @BeforeAll
+    static void configure() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "2560x1440";
+    }
+}
